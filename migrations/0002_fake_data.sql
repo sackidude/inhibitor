da00e3c80a746d1cfb3bc6a20879d71fa0f79af4
sackidude <nils.nilsen9@outlook.com>
2024-03-27 19:18:30 +0100

    account fake data

--- migrations/0002_fake_data.sql
+++ migrations/0002_fake_data.sql
@@ -4,3 +4,18 @@ VALUES
     ('nils', '123'),
     ('teo', '321'),
     ('jinghan', 'fuck');
+
+INSERT INTO
+    account (in_game_name, region, tag)
+VALUES
+    ('Chaoborus', 'EUW', 'Spec'),
+    ('Hide on bush', 'KR', 'KR1'),
+    ('C9 Berserker', 'NA', 'NA1');
+
+INSERT INTO
+    user_account (user_id, account_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (3, 2);
