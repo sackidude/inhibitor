2f05b69ab8a200324a38a13f9459616bbe583c9e
sackidude <nils.nilsen9@outlook.com>
2024-03-27 23:05:00 +0100

    slight refactoring

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,22 +1,24 @@
 use axum::routing;
+use handlers::{auth, public};
 
 mod handlers;
 mod middleware;
 
 pub fn get_router(pool: &sqlx::Pool<sqlx::Postgres>) -> axum::Router {
     let public_routes = axum::Router::new()
-        .route("/", routing::get(handlers::public::startpage::get))
-        .route("/login", routing::post(handlers::public::login::post));
+        .route("/", routing::get(public::startpage::get))
+        .route("/login", routing::post(public::login::post));
 
     let auth_routes = axum::Router::new()
-        .route("/profile", routing::get(handlers::auth::profile::get))
+        .route("/profile", routing::get(auth::profile::get))
+        .route("/accounts", routing::post(auth::accounts::post))
         .route_layer(axum::middleware::from_fn_with_state(
             pool.clone(),
             middleware::require_auth,
         ));
 
     axum::Router::new()
-        .route("/", routing::get(handlers::public::startpage::get))
+        .route("/", routing::get(public::startpage::get))
         .nest("/public", public_routes)
         .nest("/auth", auth_routes)
         .layer(tower_cookies::CookieManagerLayer::new())
