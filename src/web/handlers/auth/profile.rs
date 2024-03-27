580024eda97f2de2c2390c8dab70a7746c075f92
sackidude <nils.nilsen9@outlook.com>
2024-03-27 19:20:55 +0100

    dynamic profile implementation

--- src/web/handlers/auth/profile.rs
+++ src/web/handlers/auth/profile.rs
@@ -1,20 +1,53 @@
 use askama::Template;
-use axum::{extract, response};
+use axum::{
+    extract,
+    response::{self, IntoResponse},
+};
+use sqlx::prelude::FromRow;
+
+use crate::web::middleware;
+
+#[derive(FromRow)]
+struct Account {
+    name: String,
+    id: i32,
+}
 
 #[derive(askama::Template)]
 #[template(path = "profile.html")]
-struct ProfileTemplate<'a> {
-    username: &'a str,
-    accounts: Vec<&'a str>,
+struct ProfileTemplate {
+    accounts: Vec<Account>,
 }
 
 pub async fn get(
     extract::State(pool): extract::State<sqlx::Pool<sqlx::Postgres>>,
     cookies: tower_cookies::Cookies,
 ) -> impl response::IntoResponse {
-    let profile = ProfileTemplate {
-        username: "hello",
-        accounts: vec!["test1", "test2"],
+    // parse token
+    let token = match middleware::get_auth_token(cookies) {
+        Some(token) => token,
+        None => panic!("Request should have valid auth token after middleware"), // TODO FIX THIS
     };
+
+    let user_id = token.get_user_id();
+
+    // unchecked because it thinks the concat returns optional string.
+    let accounts = sqlx::query_as_unchecked!(
+        Account,
+        r#"SELECT
+        CONCAT(account.in_game_name, '#', account.tag) "name",
+        account.id
+    FROM
+        account
+        INNER JOIN user_account ON account.id = user_account.account_id
+    WHERE
+        user_account.user_id = ($1)"#,
+        user_id
+    )
+    .fetch_all(&pool)
+    .await
+    .unwrap();
+
+    let profile = ProfileTemplate { accounts };
     response::Html(profile.render().unwrap())
 }
