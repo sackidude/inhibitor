08ffb2f489ca31abdbf57931c1cbbeb7d6594cc8
sackidude <nils.nilsen9@outlook.com>
2024-03-27 17:27:03 +0100

    testing from str, make sure not panics

--- src/web/middleware/token.rs
+++ src/web/middleware/token.rs
@@ -1,10 +1,10 @@
-use std::{alloc::System, time::SystemTime};
+use std::{alloc::System, os::windows::raw::HANDLE, time::SystemTime};
 
 use rand::{distributions, Rng};
 
 pub const AUTH_TOKEN: &str = "auth-token";
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct AuthToken {
     user_id: i32,
     expiration: u64, // UNIX EPOCH TIME STAMP as u64
@@ -23,13 +23,25 @@ impl AuthToken {
     }
 
     pub fn from_str(token: &str) -> Option<Self> {
-        let mut iter = token.split("-");
-        let user_id = match iter.nth(1) {
-            Some(id) => id.parse().unwrap(),
+        let mut iter = token.split_inclusive("-");
+        if iter.next() != Some("user-") {
+            return None;
+        }
+        let user_id = match iter.next() {
+            Some(id) => match remove_last_char(id).parse() {
+                Ok(num) => num,
+                Err(_) => return None,
+            },
             None => return None,
         };
         let expiration = match iter.next() {
-            Some(expiration) => expiration.parse().unwrap(),
+            Some(expiration) => {
+                let parsed = remove_last_char(expiration).parse::<u64>();
+                match parsed {
+                    Ok(a) => a,
+                    Err(_) => {return None;},
+                }
+            },
             None => return None,
         };
         let signature = iter.collect::<Vec<&str>>().join("");
@@ -61,7 +73,44 @@ impl AuthToken {
     }
 }
 
+fn remove_last_char(string: &str) -> &str {
+    let mut chars = string.chars();
+    chars.next_back();
+    chars.as_str()
+}
+
+#[test]
+fn rem_last_char_test() {
+    assert_eq!(remove_last_char("abc"), "ab");
+    assert_eq!(remove_last_char(""), "");
+    assert_eq!(remove_last_char("abä"), "ab");
+}
+
 fn generate_signature() -> String {
     let mut rng = rand::thread_rng();
     (0..20).map(|_| rng.sample(distributions::Alphanumeric) as char).collect()
+}
+
+#[test]
+fn sig_length() {
+    let sig = generate_signature();
+
+    assert_eq!(sig.len(), 20)
+}
+
+#[test]
+fn normal_token_with_minus_in() {
+    let token = AuthToken::from_str("user-1-12-a-b").unwrap();
+
+    assert_eq!(token.signature, "a-b");
+    assert_eq!(token.user_id, 1);
+    assert_eq!(token.expiration, 12);
+}
+
+#[test]
+fn none_tokens_from_str() {
+    assert_eq!(AuthToken::from_str(""), None);
+    assert_eq!(AuthToken::from_str("user"), None);
+    assert_eq!(AuthToken::from_str("user-1"), None);
+    assert_eq!(AuthToken::from_str("user-1-1"), None);
 }
